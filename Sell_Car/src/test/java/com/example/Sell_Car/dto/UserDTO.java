a17341d470eeb621773747ca3d4d04389f8205a5
GK Malinda <164718081+krishaCode@users.noreply.github.com>
2025-10-13 23:30:08 +0530

    Add User entity, DTO, and UserRole enum for tests
    Introduces User entity implementing UserDetails, UserDTO for data transfer, and UserRole enum to support user roles in test sources. These additions enable testing of user-related functionality and role-based access.

---
+++ Sell_Car/src/test/java/com/example/Sell_Car/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package com.example.Sell_Car.dto;
+
+import lombok.Data;
+
+@Data
+
+public class UserDTO {
+    private long id;
+    private  String name;
+    private String email;
+    private String userRole;
+}
---
+++ Sell_Car/src/test/java/com/example/Sell_Car/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.example.Sell_Car.enums;
+
+public enum UserRole {
+    ADMIN,
+    CUSTOMER
+}
