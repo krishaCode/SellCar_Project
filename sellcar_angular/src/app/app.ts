450de0b919d195d33ddae433971df479d6258a27
GK Malinda <164718081+krishaCode@users.noreply.github.com>
2025-10-27 16:38:28 +0530

    Add dynamic navbar for admin and customer login
    Introduces conditional navigation headers in the app template based on admin or customer login status. Updates component to track login state using Storage service and Angular router events for dynamic UI changes.

--- sellcar_angular/src/app/app.ts
+++ sellcar_angular/src/app/app.ts
@@ -1,7 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule, Event, NavigationEnd } from '@angular/router';
 import { AdminRoutingModule } from "./modules/admin/admin-routing-module";
 import { NzFormModule } from "ng-zorro-antd/form";
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { Storage } from './auth/auth-components/services/storage/storage';
 
 
 @Component({
@@ -9,8 +14,23 @@ import { NzFormModule } from "ng-zorro-antd/form";
   standalone: true,
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
-  imports: [AdminRoutingModule, NzFormModule]
+  imports: [CommonModule, RouterModule, AdminRoutingModule, NzFormModule, NzLayoutModule, NzButtonModule, NzGridModule]
 })
 export class App {
   protected readonly title = signal('sellcar_angular');
+
+  isCustomerLoggedIn: boolean = Storage.isCustomerLoggedIn();
+  isAdminLoggedIn: boolean = Storage.isAdminLoggedIn();
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.isCustomerLoggedIn = Storage.isCustomerLoggedIn();
+        this.isAdminLoggedIn = Storage.isAdminLoggedIn();
+      }
+    });
+  }
+
 }
