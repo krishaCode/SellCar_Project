cef81d378e1d6a4455684ffd3714d0deb536bca7
GK Malinda <164718081+krishaCode@users.noreply.github.com>
2025-10-30 21:50:53 +0530

    Add car posting method to Admin service
    Introduced postCar method to Admin service for posting car data to the backend API. Added HTTP authorization header creation using token from Storage. Imports and constants updated to support new functionality.

--- sellcar_angular/src/app/modules/admin/services/admin.ts
+++ sellcar_angular/src/app/modules/admin/services/admin.ts
@@ -1,8 +1,28 @@
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Storage } from '../../../auth/auth-components/services/storage/storage';
+
+const BASIC_URL = "http://localhost:8080";
 
 @Injectable({
   providedIn: 'root'
 })
 export class Admin {
-  
+  constructor (private http: HttpClient){}
+
+  postCar(carDto:any):Observable<any>{
+    return this.http.post(BASIC_URL+"/api/admin/car",carDto, {
+    headers:this.createAuthoricationHeader()
+    });
+  }
+
+  createAuthoricationHeader(): HttpHeaders{
+    let authHeaders : HttpHeaders = new HttpHeaders();
+    return authHeaders.set(
+      'Authorization',
+      'Bearer ' + Storage.getToken()
+    );
+  }
 }
